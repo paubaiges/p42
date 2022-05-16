3c02c8c87ec485450928b53b054b1f16e475e2fb
Simone <simonegigante17@gmail.com>
2022-05-16 22:11:45 +0200

    Update Game.js

--- frontend/src/Game.js
+++ frontend/src/Game.js
@@ -4,6 +4,13 @@ function Game(){
     return(
         <div>
             <h1>Play the game!</h1>
+            <h2>Rhythm number 1</h2>
+            <button type="button">Ranking</button>
+            <button type="button">Home</button>
+            <br></br>
+            <br></br>
+            <button type="button">REC</button>
+            <h2>Score</h2>
         </div>
     );
 }
